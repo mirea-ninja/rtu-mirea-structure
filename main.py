5cc01f70e722502e52f210898998772a9c395048
Валерий <70258211+ValeryVerkhoturov@users.noreply.github.com>
2022-08-25 18:27:52 +0300

    Reformat code

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from collections import defaultdict
 from types import SimpleNamespace
 from mdutils import MdUtils
 
-EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME = "educational_and_scientific_structural_divisions"
+EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME = (
+    "educational_and_scientific_structural_divisions"
+)
 BRANCHES_DIRECTORY_NAME = "branches"
 
 
@@ -25,31 +27,41 @@ def text_fenced_code_block(code):
 
 
 def from_tex_to_text(tex_source):
-    return tex_source \
-        .replace("~", " ") \
-        .replace("---", "—") \
-        .replace("--", "–") \
-        .replace("<<", "«") \
-        .replace(">>", "»") \
+    return (
+        tex_source.replace("~", " ")
+        .replace("---", "—")
+        .replace("--", "–")
+        .replace("<<", "«")
+        .replace(">>", "»")
+        .replace("‘‘", "“")
+        .replace("‘‘", "“")
+        .replace("’’", "”")
         .replace("\\textnumero", "№")
+    )
 
 
 def from_tex_to_html(tex_source):
-    return tex_source \
-        .replace("~", "&nbsp;") \
-        .replace("---", "&mdash;") \
-        .replace("--", "&#8211;") \
-        .replace("<<", "&laquo;") \
-        .replace(">>", "&raquo;") \
+    return (
+        tex_source.replace("~", "&nbsp;")
+        .replace("---", "&mdash;")
+        .replace("--", "&#8211;")
+        .replace("<<", "&laquo;")
+        .replace(">>", "&raquo;")
+        .replace("‘‘", "“")
+        .replace("’’", "”")
         .replace("\\textnumero", "&numero;")
+    )
 
 
 def from_tex_to_tex_with_dirtytalk(tex_source):
-    return tex_source \
-        .replace("<<", "\\say{") \
-        .replace(">>", "}") \
-        .replace("„", "\\say{") \
+    return (
+        tex_source.replace("<<", "\\say{")
+        .replace(">>", "}")
+        .replace("„", "\\say{")
         .replace("“", "}")
+        .replace("‘‘", "\\say{")
+        .replace("’’", "}")
+    )
 
 
 def add_code_sources_to_md_file(md_file, tex_source):
@@ -57,7 +69,7 @@ def add_code_sources_to_md_file(md_file, tex_source):
         "plain text": from_tex_to_text(tex_source),
         "TeX": tex_source,
         "TeX with dirtytalk package": from_tex_to_tex_with_dirtytalk(tex_source),
-        "HTML": from_tex_to_html(tex_source)
+        "HTML": from_tex_to_html(tex_source),
     }
 
     grouped_code = defaultdict(list)
@@ -98,11 +110,12 @@ def generate_university_md_file(rtu_mirea_structure, path: Path):
                 str(
                     Path(
                         EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME,
-                        f"{from_tex_to_text(division.short_name).replace(' ', '%20')}.md")
+                        f"{from_tex_to_text(division.short_name).replace(' ', '%20')}.md",
+                    )
                 ),
-                from_tex_to_html(division.full_name)
+                from_tex_to_html(division.full_name),
             ),
-            rtu_mirea_structure.educational_and_scientific_structural_divisions
+            rtu_mirea_structure.educational_and_scientific_structural_divisions,
         )
     )
 
@@ -113,10 +126,12 @@ def generate_university_md_file(rtu_mirea_structure, path: Path):
                 str(
                     Path(
                         BRANCHES_DIRECTORY_NAME,
-                        f"{from_tex_to_text(branch.short_name).replace(' ', '%20')}.md")),
-                from_tex_to_html(branch.short_name)
+                        f"{from_tex_to_text(branch.short_name).replace(' ', '%20')}.md",
+                    )
+                ),
+                from_tex_to_html(branch.short_name),
             ),
-            rtu_mirea_structure.branches
+            rtu_mirea_structure.branches,
         )
     )
 
@@ -125,61 +140,66 @@ def generate_university_md_file(rtu_mirea_structure, path: Path):
         "Структура РТУ МИРЭА": "https://www.mirea.ru/about/the-structure-of-the-university/",
         "ГОСТ 8.417-2002": "https://ru.wikisource.org/wiki/ГОСТ_8.417‒2002",
         "Википедия. Неразрывный пробел": "https://ru.wikipedia.org/wiki/Неразрывный_пробел",
-        "А.&nbsp;Лебедев «Ководство. §&nbsp;97. Тире, минус и&nbsp;дефис, или Черты русской типографики":
-            "https://www.artlebedev.ru/kovodstvo/sections/97/",
-        "А.&nbsp;Лебедев «Ководство. §&nbsp;158. Короткое тире»": "https://www.artlebedev.ru/kovodstvo/sections/158/"
+        "А.&nbsp;Лебедев «Ководство. §&nbsp;97. Тире, минус и&nbsp;дефис, или Черты русской типографики": "https://www.artlebedev.ru/kovodstvo/sections/97/",
+        "А.&nbsp;Лебедев «Ководство. §&nbsp;158. Короткое тире»": "https://www.artlebedev.ru/kovodstvo/sections/158/",
     }
-    md_file.new_list(list(map(lambda key: md_file.new_reference_link(sources[key], key), sources)))
+    md_file.new_list(
+        list(map(lambda key: md_file.new_reference_link(sources[key], key), sources))
+    )
 
     md_file.create_md_file()
 
 
-def generate_education_and_scientific_structural_divisions_md_file(division_structure, path: Path):
-    path = Path(path, EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME)
-    path.mkdir(parents=True, exist_ok=True)
-
-    md_file = MdUtils(file_name=str(Path(
-        path,
-        f"{from_tex_to_text(division_structure.short_name)}.md")))
-
-    md_file.new_header(1, from_tex_to_html(division_structure.full_name))
-
+def generate_division_or_brench_files(structure, md_file):
     md_file.new_header(2, "Полное название")
-    add_code_sources_to_md_file(md_file, division_structure.full_name)
+    add_code_sources_to_md_file(md_file, structure.full_name)
     md_file.new_header(2, "Короткое название")
-    add_code_sources_to_md_file(md_file, division_structure.short_name)
+    add_code_sources_to_md_file(md_file, structure.short_name)
 
-    if len(division_structure.departments) > 0:
+    if len(structure.departments) > 0:
         md_file.new_header(2, "Структура")
-        for department in division_structure.departments:
-            md_file.new_header(3, from_tex_to_html(department[0].upper() + department[1:]))
+        for department in structure.departments:
+            md_file.new_header(
+                3, from_tex_to_html(department[0].upper() + department[1:])
+            )
             add_code_sources_to_md_file(md_file, department)
 
     md_file.create_md_file()
+
+
+def generate_education_and_scientific_structural_divisions_md_file(
+    division_structure, path: Path
+):
+    path = Path(path, EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME)
+    path.mkdir(parents=True, exist_ok=True)
+
+    md_file = MdUtils(
+        file_name=str(
+            Path(path, f"{from_tex_to_text(division_structure.short_name)}.md")
+        )
+    )
+
+    md_file.new_header(1, from_tex_to_html(division_structure.full_name))
+
+    generate_division_or_brench_files(division_structure, md_file)
 
 
 def generate_branches_md_file(branch_structure, path: Path):
     path = Path(path, BRANCHES_DIRECTORY_NAME)
     path.mkdir(parents=True, exist_ok=True)
 
-    md_file = MdUtils(file_name=str(Path(
-        path,
-        f"{from_tex_to_text(branch_structure.short_name)}.md")))
+    md_file = MdUtils(
+        file_name=str(Path(path, f"{from_tex_to_text(branch_structure.short_name)}.md"))
+    )
 
-    md_file.new_header(1, from_tex_to_html(branch_structure.short_name[0].upper() + branch_structure.short_name[1:]))
+    md_file.new_header(
+        1,
+        from_tex_to_html(
+            branch_structure.short_name[0].upper() + branch_structure.short_name[1:]
+        ),
+    )
 
-    md_file.new_header(2, "Полное название")
-    add_code_sources_to_md_file(md_file, branch_structure.full_name)
-    md_file.new_header(2, "Короткое название")
-    add_code_sources_to_md_file(md_file, branch_structure.short_name)
-
-    if len(branch_structure.departments) > 0:
-        md_file.new_header(2, "Структура")
-        for department in branch_structure.departments:
-            md_file.new_header(3, from_tex_to_html(department[0].upper() + department[1:]))
-            add_code_sources_to_md_file(md_file, department)
-
-    md_file.create_md_file()
+    generate_division_or_brench_files(branch_structure, md_file)
 
 
 def generate_laguage_specific_structure(rtu_mirea_structure, lang: str):
@@ -187,30 +207,46 @@ def generate_laguage_specific_structure(rtu_mirea_structure, lang: str):
     path = Path(lang)
 
     generate_university_md_file(rtu_mirea_structure_lang, path)
-    for division in rtu_mirea_structure_lang.educational_and_scientific_structural_divisions:
+    for (
+        division
+    ) in rtu_mirea_structure_lang.educational_and_scientific_structural_divisions:
         generate_education_and_scientific_structural_divisions_md_file(division, path)
     for branch in rtu_mirea_structure_lang.branches:
         generate_branches_md_file(branch, path)
 
 
 def main():
-    rtu_mirea_structure = json.loads(open("rtu-mirea-structure.json").read(),
-                                     object_hook=lambda d: SimpleNamespace(**d))
+    rtu_mirea_structure = json.loads(
+        open("rtu-mirea-structure.json").read(),
+        object_hook=lambda d: SimpleNamespace(**d),
+    )
 
-    for lang, _ in rtu_mirea_structure.__dict__.items():
+    for lang in rtu_mirea_structure.__dict__.keys():
         generate_laguage_specific_structure(rtu_mirea_structure, lang)
 
     with open("ru_RU/README.md", "r") as rm_ru_file:
         rm_file = MdUtils(file_name="README.md")
 
-        rm_file.new_paragraph(rm_file.new_reference_link("en_US/README.md", "English names"))
-        rm_file.new_paragraph(rm_ru_file.read()
-                              .replace(EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME,
-                                       str(Path("ru_RU",
-                                                EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME)))
-                              .replace(BRANCHES_DIRECTORY_NAME, str(Path("ru_RU", BRANCHES_DIRECTORY_NAME))))
+        rm_file.new_paragraph(
+            rm_file.new_reference_link("en_US/README.md", "English names")
+        )
+        rm_file.new_paragraph(
+            rm_ru_file.read()
+            .replace(
+                EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME,
+                str(
+                    Path(
+                        "ru_RU",
+                        EDUCATIONAL_AND_SCIENTIFIC_STRUCTURAL_DIVISIONS_DIRECTORY_NAME,
+                    )
+                ),
+            )
+            .replace(
+                BRANCHES_DIRECTORY_NAME, str(Path("ru_RU", BRANCHES_DIRECTORY_NAME))
+            )
+        )
         rm_file.create_md_file()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
